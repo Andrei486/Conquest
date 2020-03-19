5a911f61a1e63d224f541e654635a0d23e912ea9
Andrei486 <55627866+Andrei486@users.noreply.github.com>
2020-03-19 12:04:42 -0400

    commit before branching

--- Assets/Scripts/SaveManager.cs
+++ Assets/Scripts/SaveManager.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SaveManager : MonoBehaviour
 {
+    public bool isBattleSaved = false;
     //file paths
-    public const string saveFile = "/Assets/Miscellaneous/saveFile.txt";
+    public const string battleSave = "/Assets/Miscellaneous/saveFile.txt";
     public const string backupFile = "/Assets/Miscellaneous/backupFile.txt";
     public const string baseUnits = "/Assets/Miscellaneous/baseUnits.txt";
     void Start()
@@ -18,17 +19,30 @@ public class SaveManager : MonoBehaviour
         
     }
 
-    public void LoadFile(){
+    public void LoadBattle(string mapData = battleSave){
+        /**Loads the battlefield and units corresponding to the JSON file in mapData.
+        If no argument is given, loads the battle save instead, assuming it exists.*/
+
+        //get relevant JSON objects
+
+        //create the map by calling CreateMap()
+        //map must be in the Resources folder
 
     }
 
-    public void SaveFile(){
-        File.Copy(saveFile, backupFile);
-        File.WriteAllText(saveFile, string.Empty);
+    public void BattleSave(){
+        File.Copy(battleSave, backupFile); //back up the save
+        File.WriteAllText(battleSave, string.Empty); //empty the save
+
+        //write map name (and other info: event flags?) in a map object
+
+        //write units and positions (this ignores and overwrites the information in the map data)
+
+        isBattleSaved = true;
     }
 
     public void CreateDefaultFile(){
         /**Creates a basic save file.*/
-        File.Copy(baseUnits, saveFile);
+        File.Copy(baseUnits, battleSave);
     }
 }
